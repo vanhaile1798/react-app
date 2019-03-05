8c70523ee7fecfdc7409a8c1f67bd3cdfd9fd5ee
vanhaile1798 <vanhaile1798@gmail.com>
2019-03-05 13:22:12 +0700

    Updates

---
+++ precache-manifest.884da48ab224b6f17a8537648d304ff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "e2b4d2a9f9871cb9b856",
+    "url": "/react-app/static/js/main.e2b4d2a9.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/react-app/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "e2b4d2a9f9871cb9b856",
+    "url": "/react-app/static/css/main.9a99916a.chunk.css"
+  },
+  {
+    "revision": "44103023e59e9c08daa7ce4be8e64ae2",
+    "url": "/react-app/index.html"
+  }
+];
